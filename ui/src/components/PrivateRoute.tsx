6f66a14fdca0cb815acf5358ea59f820666af629
Etha-Arrowood <ethan.arrowood@gmail.com>
2018-12-20 16:18:27 -0500

    Properly check for authResponse and reset if authenticated

--- ui/src/components/PrivateRoute.tsx
+++ ui/src/components/PrivateRoute.tsx
@@ -17,6 +17,8 @@ export class PrivateRoute extends React.Component<IPrivateRouteProps> {
   public componentDidUpdate(prevProps, prevState, snapshot) {
     if (this.context.accessToken === null && this.props.path !== prevProps.path) {
       this.context.setAuthResponse(`Please log in to access: ${this.props.path}`)
+    } else if (this.context.authResponse !== null && this.context.accessToken !== null) {
+      this.context.setAuthResponse(null)
     }
   }
 
