fb9c8e29c459ea4480cf67222aa56448c6fd927d
bengreenier <begreen@microsoft.com>
2019-01-16 15:16:09 -0500

    add backend tests

--- ui/integration-test-tool/README.md
+++ ui/integration-test-tool/README.md
@@ -5,7 +5,8 @@ This tool runs integration tests against production instances.
 ## How
 
 + `npm i`
-+ `set TARGET_SITE=<siteBaseUrl>`
++ `set TARGET_BACKEND_SITE=<siteBaseUrl>`
++ `set TARGET_FRONTEND_SITE=<siteBaseUrl>`
 + `set TARGET_USERNAME=<aadUsername>`
 + `set TARGET_PASSWORD=<aadPassword>`
 + `npm test`
--- ui/integration-test-tool/config.ts
+++ ui/integration-test-tool/config.ts
@@ -1,5 +1,6 @@
 export const config = Object.freeze({
+  backend_site: process.env.TARGET_BACKEND_SITE || 'http://bing.com',
   password: process.env.TARGET_PASSWORD || '1234567',
-  site: process.env.TARGET_SITE || 'http://bing.com',
+  ui_site: process.env.TARGET_FRONTEND_SITE || 'http://bing.com',
   username: process.env.TARGET_USERNAME || 'alice',
 })
